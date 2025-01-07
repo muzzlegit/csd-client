751d2fef5dbb541e96fdc0766b2a1c678a28e8f5
Роман <muzzlestore@gmail.com>
2025-01-07 23:12:00 +0200

    add aluminum busbars module

--- src/modules/info/compose/InfoModule/InfoModule.jsx
+++ src/modules/info/compose/InfoModule/InfoModule.jsx
@@ -1,4 +1,5 @@
 import BusbarsCurrent from "modules/info/components/BusbarsCurrent/BusbarsCurrent";
+import aluminumData from "../../data/aluminum.json";
 import copperData from "../../data/copper.json";
 import { Container } from "./InfoModule.styled";
 
@@ -6,6 +7,7 @@ const InfoModule = () => {
   return (
     <Container>
       <BusbarsCurrent type="copper" busbarData={copperData} />
+      <BusbarsCurrent type="aluminum" busbarData={aluminumData} />
     </Container>
   );
 };
--- src/modules/info/compose/InfoModule/InfoModule.styled.js
+++ src/modules/info/compose/InfoModule/InfoModule.styled.js
@@ -3,4 +3,6 @@ import styled from "@emotion/styled";
 export const Container = styled.div({
   width: "100%",
   height: "100%",
+  display: "flex",
+  gap: "16px",
 });
