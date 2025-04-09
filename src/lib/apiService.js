7cab755349a15b030488851fd9e005d0b401b8d3
Роман <muzzlestore@gmail.com>
2025-04-09 12:27:04 +0300

    fix url

--- src/lib/apiService.js
+++ src/lib/apiService.js
@@ -1,25 +1,28 @@
 export const fetchItemByArticle = async (article) => {
   try {
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        searchval: article,
-        categories: [0],
-        brands: [],
-        characteristics: [],
-        filters: [],
-        perPage: 10,
-        page: 1,
-        sortBy: null,
-        sortOrder: null,
-        balance: true,
-        custom_brand_view: false,
-        brand_view: false,
-      }),
-    });
+    const response = await fetch(
+      "https://services.vector-vs.com/price-list-internal/products",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          searchval: article,
+          categories: [0],
+          brands: [],
+          characteristics: [],
+          filters: [],
+          perPage: 10,
+          page: 1,
+          sortBy: null,
+          sortOrder: null,
+          balance: true,
+          custom_brand_view: false,
+          brand_view: false,
+        }),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Помилка в запиті: ${response.status}`);
@@ -32,9 +35,12 @@ export const fetchItemByArticle = async (article) => {
       return null;
     }
 
-    const productResponse = await fetch(`/api/product/${remote_id}`, {
-      method: "GET",
-    });
+    const productResponse = await fetch(
+      `https://services.vector-vs.com/price-list-internal/product/${remote_id}`,
+      {
+        method: "GET",
+      }
+    );
 
     if (!productResponse.ok) {
       throw new Error(`HTTP помилка: ${productResponse.status}`);
@@ -47,6 +53,56 @@ export const fetchItemByArticle = async (article) => {
     return null;
   }
 };
+
+// export const fetchItemByArticle = async (article) => {
+//   try {
+//     const response = await fetch("/api/products", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         searchval: article,
+//         categories: [0],
+//         brands: [],
+//         characteristics: [],
+//         filters: [],
+//         perPage: 10,
+//         page: 1,
+//         sortBy: null,
+//         sortOrder: null,
+//         balance: true,
+//         custom_brand_view: false,
+//         brand_view: false,
+//       }),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`Помилка в запиті: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     const remote_id = data?.data?.data?.[0]?.remote_id;
+
+//     if (!remote_id) {
+//       return null;
+//     }
+
+//     const productResponse = await fetch(`/api/product/${remote_id}`, {
+//       method: "GET",
+//     });
+
+//     if (!productResponse.ok) {
+//       throw new Error(`HTTP помилка: ${productResponse.status}`);
+//     }
+
+//     const productData = await productResponse.json();
+//     return productData;
+//   } catch (error) {
+//     console.error("Помилка під час виконання запиту:", error);
+//     return null;
+//   }
+// };
 
 export const fetchItemsByCategory = async (category) => {
   try {
