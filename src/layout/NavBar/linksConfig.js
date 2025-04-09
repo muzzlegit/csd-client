c06c3419126cfec9d1ccd1ce1cd4c3854078a4ba
Роман <muzzlestore@gmail.com>
2025-04-09 12:28:57 +0300

    remove secret

--- src/layout/NavBar/linksConfig.js
+++ src/layout/NavBar/linksConfig.js
@@ -1,9 +1,4 @@
-import {
-  GiAbstract095,
-  GiAncientColumns,
-  GiBalloonDog,
-  GiDualityMask,
-} from "react-icons/gi";
+import { GiAbstract095, GiBalloonDog } from "react-icons/gi";
 
 export const links = [
   { icon: GiAbstract095, path: "/", title: "Загальні дані", protected: false },
@@ -13,16 +8,16 @@ export const links = [
     title: "Перемикачі",
     protected: false,
   },
-  {
-    icon: GiAncientColumns,
-    path: "/terminal",
-    title: "Клеми",
-    protected: false,
-  },
-  {
-    icon: GiDualityMask,
-    path: "/admin-panel",
-    title: "адмінка",
-    protected: true,
-  },
+  // {
+  //   icon: GiAncientColumns,
+  //   path: "/terminal",
+  //   title: "Клеми",
+  //   protected: false,
+  // },
+  // {
+  //   icon: GiDualityMask,
+  //   path: "/admin-panel",
+  //   title: "адмінка",
+  //   protected: true,
+  // },
 ];
